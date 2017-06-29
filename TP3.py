e137e50209c7cbfdc813f04eb822750593adf059
Cheiras <dtcheche@gmail.com>
2017-06-29 18:03:12 -0300

    Final

--- TP3.py
+++ TP3.py
@@ -3,179 +3,7 @@ import sys
 import heapq
 import time
 from TDAs import *
-
-def procesar_archivo(grafo, archivo):
-	"""Función que abre el archivo y linea por linea va generando vertices y aristas."""
-	lineas_de_cabecera = 4
-	try:
-		with open(archivo, "r") as archivo:
-			i = 0
-			for linea in archivo:
-				if (i <= lineas_de_cabecera):
-					i += 1
-					continue
-				id_1, id_2 = linea.rstrip("\n").split("\t")
-				vertice_1 = Vertice(id_1, id_1)
-				vertice_2 = Vertice(id_2, id_2)
-				grafo.agregar_vertice(vertice_1)
-				grafo.agregar_vertice(vertice_2)
-				grafo.agregar_arista(vertice_1, vertice_2)
-	except FileNotFoundError:
-		print("El archivo no fue creado aún.")
-		return False
-	except IOError:
-		print("Error al intentar abrir o guardar el archivo.")
-		return False
-	return True
-
-def generar_grafo(archivo):
-	"""Función que crea un grafo y le agrega todos los vertices y aristas."""
-	grafo = Grafo()
-	if not procesar_archivo(grafo, archivo):
-		return False
-	return grafo
-
-def get_rand_vert(lista):
-	"""Devuelve una clave aleatoria del diccionario.
-	Pre: El diccionario no esta vacio"""
-	posicion = random.randint(0, len(lista)-1)
-	return lista[posicion]
-
-def validar_vertice(vertice, grafo):
-	"""Verifica si el vértice se encuentra en el grafo."""
-	if vertice == -1:
-		print("El vertice no se encuentra en el grafo.")
-		return False
-	return True
-
-def validar_cantidad(cantidad, grafo):
-	"""Verifica que el numero pasado por parametro esté dentro del rango del grafo."""
-	return cantidad < grafo.cantidad_vertices()
-
-def verificar_elecciones(vertice, n, grafo):
-	"""Verifica las elecciones pasadas por parametro."""
-	if not validar_cantidad(n, grafo):
-		print("Ingreso una cantidad mayor a la del grafo.")
-		return False
-	return validar_vertice(vertice, grafo)
-
-def establecer_cantidad(n, grafo):
-	"""Establece la cantidad de random walks a hacer según el numero pasado por parametro."""
-	cantidad = n*100
-	if not validar_cantidad(cantidad, grafo):
-		cantidad = grafo.cantidad_vertices()
-	return cantidad
-
-def random_walk(largo, cantidad, vertice, grafo):
-	"""Genera un random walk."""
-	if not validar_cantidad(largo, grafo):
-		return -1
-	apariciones = {}
-	v = vertice
-	for c in range(cantidad):
-		i = 0
-		while i < largo:
-			w = get_rand_vert(list(grafo.adyacentes_vertice(v).values()))
-			cant = apariciones.get(w.iden, 0)
-			apariciones[w.iden] = cant+1  
-			i += 1
-			v = w
-	return apariciones
-
-def crear_heap_menores(diccionario, largo):
-	"""Genera un heap de menores de largo dado lleno de vertices similares 
-	al recibido por parametro."""
-	heap_min = []
-	heapq.heapify(heap_min)
-	for vertice in list(diccionario.keys()):
-		tupla = (diccionario[vertice], vertice)
-		if len(heap_min) < largo:
-			heapq.heappush(heap_min, tupla)
-		else:
-			if heap_min[0][0] < tupla[0]:
-				heapq.heapreplace(heap_min,tupla)
-	return heap_min
-
-def recorrido_BFS(grafo, origen, destino):
-	"""Hace recorrido BFS desde un origen hasta un destino si se desea agregarle."""
-	visitados = {}
-	padre = {}
-	orden = {}
-	padre[origen] = None
-	orden[origen] = 0
-	q = Cola()
-	q.encolar(origen)
-	visitados[origen] = True
-	while not q.esta_vacia():
-		v = q.desencolar()
-		vertice = grafo.obtener_vertice(v)
-		for w in grafo.adyacentes_vertice(vertice):
-			if w not in visitados:
-				visitados[w] = True
-				padre[w] = v
-				orden[w] = orden[v] + 1
-				if(destino != None ):
-					if(w == destino):
-						break;
-				q.encolar(w)
-	return padre, orden
-
-def verificar_parametros(comando, ingreso):
-	"""Verifica que los parametros ingresados por el usuario sean correctos."""
-	if len(ingreso) == 0 or len(ingreso) > 3:
-		print("Ingresó una cantidad incorrecta de parametros.")
-		return False
-	if (comando == 0):
-		print("El comando ingresado es incorrecto.")
-		return False
-	if (comando == 1 or comando == 2 or comando == 3):
-		if (len(ingreso) != 3):
-			print("Ingresó una cantidad incorrecta de parametros.")
-			return False
-	if (comando == 4 or comando == 5):
-		if (len(ingreso) != 2):
-			print("Ingresó una cantidad incorrecta de parametros.")
-			return False
-	if (comando == 6 or comando == 7 or comando == 8):
-		if (len(ingreso) != 1):
-			print("Ingresó una cantidad incorrecta de parametros.")
-			return False
-	return True
-
-def menu(grafo):
-	"""Corre el menú que interactúa con el usuario."""
-	opciones = {"similares":1, "recomendar":2, "camino":3, "centralidad":4, "distancias":5, "estadisticas":6, "comunidades":7, "s":8}
-	while True:
-		ingreso = input("Ingrese el comando deseado o 'S' para salir: ").split(" ")
-		comando = opciones.get(ingreso[0].lower(), 0)
-		if not verificar_parametros(comando, ingreso):
-			menu(grafo)
-		for i in range (len(ingreso)-1, 0, -1):
-			parametro = ingreso[i]
-			if not parametro.isdigit():
-				print("Los parametros deben ser dígitos.")
-				menu(grafo)
-		if(comando == 1):
-			similares(ingreso[1], int(ingreso[2]), grafo)
-		if(comando == 2):
-			recomendar(ingreso[1], int(ingreso[2]), grafo)
-		if(comando == 3):
-			camino(ingreso[1], ingreso[2], grafo)
-		if(comando == 4):
-			centralidad(grafo, int(ingreso[1]))
-		if(comando == 5):
-			distancia(grafo, ingreso[1])
-		if(comando == 6):
-			estadisticas(grafo)
-		if(comando == 7):
-			ini = time.time()
-			comunidades(grafo)
-			fin = time.time()
-			print("Tiempo:", (fin-ini))
-		if(comando == 8):
-			sys.exit()
-
-##########################################################################################
+from funciones_aux import *
 
 def similares(iden, n, grafo):
 	"""Dado un usuario, encontrar los personajes más similares a este."""
@@ -306,6 +134,36 @@ def comunidades(grafo):
 		print("Comunidad {}, {} integrantes: {}".format(label, len(comunidades_d[label]),comunidades_d[label]))
 	print("Cantidad de comunidades: {}".format(cantidad))
 
+
+def menu(grafo):
+	"""Corre el menú que interactúa con el usuario."""
+	opciones = {"similares":1, "recomendar":2, "camino":3, "centralidad":4, "distancias":5, "estadisticas":6, "comunidades":7, "s":8}
+	while True:
+		ingreso = input("Ingrese el comando deseado o 'S' para salir: ").split(" ")
+		comando = opciones.get(ingreso[0].lower(), 0)
+		if not verificar_parametros(comando, ingreso):
+			menu(grafo)
+		for i in range (len(ingreso)-1, 0, -1):
+			parametro = ingreso[i]
+			if not parametro.isdigit():
+				print("Los parametros deben ser dígitos.")
+				menu(grafo)
+		if(comando == 1):
+			similares(ingreso[1], int(ingreso[2]), grafo)
+		if(comando == 2):
+			recomendar(ingreso[1], int(ingreso[2]), grafo)
+		if(comando == 3):
+			camino(ingreso[1], ingreso[2], grafo)
+		if(comando == 4):
+			centralidad(grafo, int(ingreso[1]))
+		if(comando == 5):
+			distancia(grafo, ingreso[1])
+		if(comando == 6):
+			estadisticas(grafo)
+		if(comando == 7):
+			comunidades(grafo)
+		if(comando == 8):
+			sys.exit()
 
 def main():
 	"""Función que corre el programa."""
---
+++ funciones_aux.py
@@ -0,0 +1,144 @@
+import random
+import sys
+import heapq
+from TDAs import *
+
+
+def procesar_archivo(grafo, archivo):
+	"""Función que abre el archivo y linea por linea va generando vertices y aristas."""
+	lineas_de_cabecera = 4
+	try:
+		with open(archivo, "r") as archivo:
+			i = 0
+			for linea in archivo:
+				if (i <= lineas_de_cabecera):
+					i += 1
+					continue
+				id_1, id_2 = linea.rstrip("\n").split("\t")
+				vertice_1 = Vertice(id_1, id_1)
+				vertice_2 = Vertice(id_2, id_2)
+				grafo.agregar_vertice(vertice_1)
+				grafo.agregar_vertice(vertice_2)
+				grafo.agregar_arista(vertice_1, vertice_2)
+	except FileNotFoundError:
+		print("El archivo no fue creado aún.")
+		return False
+	except IOError:
+		print("Error al intentar abrir o guardar el archivo.")
+		return False
+	return True
+
+def generar_grafo(archivo):
+	"""Función que crea un grafo y le agrega todos los vertices y aristas."""
+	grafo = Grafo()
+	if not procesar_archivo(grafo, archivo):
+		return False
+	return grafo
+
+def get_rand_vert(lista):
+	"""Devuelve una clave aleatoria del diccionario.
+	Pre: El diccionario no esta vacio"""
+	posicion = random.randint(0, len(lista)-1)
+	return lista[posicion]
+
+def validar_vertice(vertice, grafo):
+	"""Verifica si el vértice se encuentra en el grafo."""
+	if vertice == -1:
+		print("El vertice no se encuentra en el grafo.")
+		return False
+	return True
+
+def validar_cantidad(cantidad, grafo):
+	"""Verifica que el numero pasado por parametro esté dentro del rango del grafo."""
+	return cantidad < grafo.cantidad_vertices()
+
+def verificar_elecciones(vertice, n, grafo):
+	"""Verifica las elecciones pasadas por parametro."""
+	if not validar_cantidad(n, grafo):
+		print("Ingreso una cantidad mayor a la del grafo.")
+		return False
+	return validar_vertice(vertice, grafo)
+
+def establecer_cantidad(n, grafo):
+	"""Establece la cantidad de random walks a hacer según el numero pasado por parametro."""
+	cantidad = n*100
+	if not validar_cantidad(cantidad, grafo):
+		cantidad = grafo.cantidad_vertices()
+	return cantidad
+
+def random_walk(largo, cantidad, vertice, grafo):
+	"""Genera un random walk."""
+	if not validar_cantidad(largo, grafo):
+		return -1
+	apariciones = {}
+	v = vertice
+	for c in range(cantidad):
+		i = 0
+		while i < largo:
+			w = get_rand_vert(list(grafo.adyacentes_vertice(v).values()))
+			cant = apariciones.get(w.iden, 0)
+			apariciones[w.iden] = cant+1  
+			i += 1
+			v = w
+	return apariciones
+
+def crear_heap_menores(diccionario, largo):
+	"""Genera un heap de menores de largo dado lleno de vertices similares 
+	al recibido por parametro."""
+	heap_min = []
+	heapq.heapify(heap_min)
+	for vertice in list(diccionario.keys()):
+		tupla = (diccionario[vertice], vertice)
+		if len(heap_min) < largo:
+			heapq.heappush(heap_min, tupla)
+		else:
+			if heap_min[0][0] < tupla[0]:
+				heapq.heapreplace(heap_min,tupla)
+	return heap_min
+
+def recorrido_BFS(grafo, origen, destino):
+	"""Hace recorrido BFS desde un origen hasta un destino si se desea agregarle."""
+	visitados = {}
+	padre = {}
+	orden = {}
+	padre[origen] = None
+	orden[origen] = 0
+	q = Cola()
+	q.encolar(origen)
+	visitados[origen] = True
+	while not q.esta_vacia():
+		v = q.desencolar()
+		vertice = grafo.obtener_vertice(v)
+		for w in grafo.adyacentes_vertice(vertice):
+			if w not in visitados:
+				visitados[w] = True
+				padre[w] = v
+				orden[w] = orden[v] + 1
+				if(destino != None ):
+					if(w == destino):
+						break;
+				q.encolar(w)
+	return padre, orden
+
+def verificar_parametros(comando, ingreso):
+	"""Verifica que los parametros ingresados por el usuario sean correctos."""
+	if len(ingreso) == 0 or len(ingreso) > 3:
+		print("Ingresó una cantidad incorrecta de parametros.")
+		return False
+	if (comando == 0):
+		print("El comando ingresado es incorrecto.")
+		return False
+	if (comando == 1 or comando == 2 or comando == 3):
+		if (len(ingreso) != 3):
+			print("Ingresó una cantidad incorrecta de parametros.")
+			return False
+	if (comando == 4 or comando == 5):
+		if (len(ingreso) != 2):
+			print("Ingresó una cantidad incorrecta de parametros.")
+			return False
+	if (comando == 6 or comando == 7 or comando == 8):
+		if (len(ingreso) != 1):
+			print("Ingresó una cantidad incorrecta de parametros.")
+			return False
+	return True
+
